0d2d4aab72f61985242cd564387f22261efbf0cd
Tanyanyanya <76079713+Tanyanyanya@users.noreply.github.com>
2024-06-13 16:53:38 +0200

    Add some common table expression

--- Subquery.sql
+++ Subquery.sql
@@ -22,3 +22,46 @@ inner join (
 	from general_hospital.patients
 	where date_of_birth <= '1999-01-01'
 ) p on se.master_patient_id = p.master_patient_id;
+
+/* Rewriting a subquery from ^ */
+
+with young_patients as (
+	select *
+	from general_hospital.patients 
+	where date_of_birth >= '2000-01-01'
+)
+select * 
+/* Instead of referring to a table in database, we'll
+refer to this common table expression */ 
+from young_patients
+where name ilike 'm%';
+
+
+/* The number of surgeries by county, for counties more than 5500 patients */ 
+with  top_counties as (
+	select
+		county,
+		count(*) as num_patients
+	from general_hospital.patients
+	group by county
+	having count(*) > 1500
+	),
+	county_patients as (
+		select
+			p.master_patient_id,
+			p.county
+		from general_hospital.patients p
+		/* Join the patients table, the common table expression
+		that we just wrote above */
+		inner join top_counties t on 
+			p.county = t.county
+	)
+select 
+	p.county,
+	count(s.surgery_id) as num_surgeries
+from general_hospital.surgical_encounters s 
+inner join county_patients p on
+	s.master_patient_id = p.master_patient_id
+group by p.county;
+
+
